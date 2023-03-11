c24c23b09d7df4c82143b037c68b6a8d6f598600
Ada Christine <adachristine18@gmail.com>
2023-03-11 19:27:43 +0000

    scaffold

---
+++ AdaChristine.Sandbox.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AdaChristine.Sandbox.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
